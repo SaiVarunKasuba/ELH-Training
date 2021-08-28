676c21dc9a931971994133a0550a0643a493fc82
saivarun777 <44079725+saivarun777@users.noreply.github.com>
2021-08-28 20:33:19 +0530

    Add files via upload
    University Project With Collections

---
+++ UniversityWithCollections/src/main/java/com/service/DepartmentServiceImpl.java
@@ -0,0 +1,36 @@
+package com.service;
+
+
+import java.util.List;
+
+import com.exception.MyException;
+import com.model.Department;
+import com.model.Student;
+
+public class DepartmentServiceImpl implements DepartmantService {
+
+	@Override
+	public Student searchStudent(Department department, int search) throws MyException{
+		
+		Student details=null;
+		List<Student> studentDetails= department.getStudents();
+		
+		for (Student student : studentDetails) {
+			if(student.getStudentId()==search)
+			{
+			details=student;
+			}
+
+		}
+		if(details!=null)
+		{
+			return details;
+		}
+		else
+		{
+			throw new MyException("No data found");
+		}
+		
+	}
+
+}
